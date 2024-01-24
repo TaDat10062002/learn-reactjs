d2f96c322f09ebd1247b2cdcae324e5d7ff206e6
Ta Dat <talinhdat03@gmail.com>
2024-01-24 23:57:54 +0700

    update for prop

---
+++ example/props_type_checking/props.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ColorBox from "./App";
+
+export default function App() {
+    return (
+        <section>
+            <ColorBox color="yellow"></ColorBox>
+            <ColorBox color="pink"></ColorBox>
+            <ColorBox color="purple"></ColorBox>
+        </section>
+    )
+}
