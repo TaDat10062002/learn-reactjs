f204729b41562251ccc799e6af94d3cbe691ec2d
Ta Dat <talinhdat03@gmail.com>
2024-01-25 00:17:10 +0700

    updated for app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ta Dat 2024 quyet dinh hoc Reactjs va Nodejs
-          Va lam do an
-        </p>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Ta Dat 2024 quyet dinh hoc Reactjs va Nodejs
+//           Va lam do an
+//         </p>
 
-      </header>
-    </div>
-  );
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+function ColorBox(props) {
+    const { color } = props; // destruturing object
+
+    return (
+        <div className="box" style={{ backgroundColor: color }}></div>
+    )
 }
 
-export default App;
+export default ColorBox;
